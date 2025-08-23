7c00d1538d3b25e7abf767c3f732aeb695581d24
Benshekniel Thayalan <101684571+Benshekniel@users.noreply.github.com>
2025-08-23 18:27:40 +0530

    update 8

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  // base: '/portfolio/', 
+  plugins: [react()], 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/portfolio/'
 });
